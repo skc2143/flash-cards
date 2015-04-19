bfdd458143b40b331bf62b85816d947f7b34a3f5
Steve Cheruiyot <skc2143@columbia.edu>
2015-04-18 21:59:31 -0400

    first commit

--- public/app.js
+++ public/app.js
@@ -26,3 +26,35 @@ app.value('whateverName', [
         ]
     }
 ]);
+
+app.controller('StatsController', function($scope, ScoreFactory) {
+    $scope.scores = ScoreFactory;
+});
+
+app.factory('FlashCardsFactory', function($http) {
+
+    return {
+
+        getFlashCards: function(category) {
+
+            var queryParams = {};
+
+            if (category) {
+                queryParams.category = category;
+            }
+
+            $http.get('\cards', {
+                params: queryParams
+            }).then(function (response) {
+                return response.data;
+            });
+        }
+    };
+});
+
+app.factory('ScoreFactory', function() {
+    return {
+        correct: 0,
+        incorrect: 0
+    };
+});
--- public/main.controller.js
+++ public/main.controller.js
@@ -1,5 +1,30 @@
-app.controller('MainController', function ($scope, whateverName) {
-	$scope.flashCards = whateverName;
+app.controller('MainController', function ($scope, FlashCardsFactory) {
+
+	var selectedCategory = 'Reset';
+	var flash = {};
+
+	$scope.categories = [
+		'MongoDB',
+		'Express',
+		'Angular',
+		'Node'
+	];
+
+	$scope.getCategoryCards = function (category) {
+		selectedCategory = category;
+	}
+
+	$scope.flashCards = function() {
+		if(selectedCategory === 'Reset') {
+			return FlashCardsFactory.getFlashCards();
+		} else {
+			return FlashCardsFactory.getFlashCards(selectedCategory);
+		}
+	};
+
+	$scope.reset = function() {
+		selectedCategory = 'Reset';
+	}
 
 	$scope.answerQuestion = function (answer, flashCard) {
 		if (!flashCard.answered) {
